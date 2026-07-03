b16411df7965e6762efd37ecac16366385559174
Engr Duaa Ali <50747588+DuaaAli28@users.noreply.github.com>
2026-07-03 12:35:34 +0500

    initial commit

---
+++ script.js
@@ -0,0 +1,21 @@
+console.log("Script Loaded");
+const display = document.querySelector(".display");
+const buttons = document.querySelectorAll("button");
+
+
+buttons.forEach((button) => {
+  button.addEventListener("click", () => {
+    if (button.textContent === "=") {
+      try{
+      display.value = eval(display.value);}
+      catch (error)
+      {
+        display.value ="Error";
+      }
+    } else if (button.textContent === "C") {
+      display.value = "";
+    } else {
+      display.value += button.textContent;
+    }
+  });
+});
